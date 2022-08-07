2929273734408de2581c5b14c7eec22e6cdb01d6
AsenTerziyski <asenterziyski@gmail.com>
2022-08-07 10:09:41 +0300

    Add some code in MainActivity

--- SimpleGetRequestbyRetrofitwithKotlinCoroutines/app/src/main/java/bg/asentt/simplegetrequestbyretrofitwithkotlincoroutines/MainActivity.kt
+++ SimpleGetRequestbyRetrofitwithKotlinCoroutines/app/src/main/java/bg/asentt/simplegetrequestbyretrofitwithkotlincoroutines/MainActivity.kt
@@ -2,10 +2,16 @@ package bg.asentt.simplegetrequestbyretrofitwithkotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val user = Utils.createInstance().create(ApiInterface::class.java)
+        val results = user.getUsers()
+        if (results != null) {
+            Log.d("TAG", "On CREATE: ${results.body().toString()}")
+        }
     }
 }

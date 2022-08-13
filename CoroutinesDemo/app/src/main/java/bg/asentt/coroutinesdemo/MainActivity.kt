5e0cb018a7482b7fdfe6a34dafc57c7e980750c5
AsenTerziyski <asenterziyski@gmail.com>
2022-08-13 11:11:30 +0300

    First init of coroutine demo app.

--- CoroutinesDemo/app/src/main/java/bg/asentt/coroutinesdemo/MainActivity.kt
+++ CoroutinesDemo/app/src/main/java/bg/asentt/coroutinesdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 //https://www.youtube.com/watch?v=kvfpuzSwVZ8
@@ -15,7 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch {
-            Log.d(TAG, "Coroutine says hello from ${Thread.currentThread().name}")
+            delay(3000L)
+            Log.d(TAG, "Coroutine says hello 1 from ${Thread.currentThread().name}")
+        }
+
+        GlobalScope.launch {
+            delay(7000L)
+            Log.d(TAG, "Coroutine says hello 2 from ${Thread.currentThread().name}")
+        }
+
+        GlobalScope.launch {
+            delay(2000L)
+            Log.d(TAG, "Coroutine says hello 3 from ${Thread.currentThread().name}")
         }
 
         Log.d(TAG, "Hello from thread ${Thread.currentThread().name }")

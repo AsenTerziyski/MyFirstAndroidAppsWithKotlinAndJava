96a022555df084a84ee8bca4d9f2458abc3b2e58
AsenTerziyski <asenterziyski@gmail.com>
2022-08-14 08:09:57 +0300

    Add lifecycleScope and viewModelScope dependencies.

--- CoroutinesDemo/app/src/main/java/bg/asentt/coroutinesdemo/MainActivity.kt
+++ CoroutinesDemo/app/src/main/java/bg/asentt/coroutinesdemo/MainActivity.kt
@@ -7,16 +7,33 @@ import android.util.Log
 import android.widget.Button
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
 
 //https://www.youtube.com/watch?v=uiPYcSSjNTI
 //https://www.youtube.com/watch?v=kvfpuzSwVZ8
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
-    var counter: Int = 0
+//    var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    GlobalScope.launch(Dispatchers.IO) {
+        val time = measureTimeMillis {
+        val answer1 = networkCall1()
+        val answer2 = networkCall2()
+        Log.d(TAG, "Answer one is $answer1")
+        Log.d(TAG, "Answer two is $answer2")
+        }
+        Log.d(TAG, "Time is $time ms.")
+    }
+
+
+
+
+
 
 //        GlobalScope.launch {
 //            delay(3000L)
@@ -58,28 +75,28 @@ class MainActivity : AppCompatActivity() {
 ////                }
 //        }
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
-//            repeat(5) {
-//                Log.d(TAG, "Coroutine is still working!")
-//                delay(1000L)
-//                Log.d(TAG, "Started long running calculation...")
-//                for(i in 30..40) {
-//                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+//        val job = GlobalScope.launch(Dispatchers.Default) {
+////            repeat(5) {
+////                Log.d(TAG, "Coroutine is still working!")
+////                delay(1000L)
+////                Log.d(TAG, "Started long running calculation...")
+////                for(i in 30..40) {
+////                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+////                }
+////                Log.d(TAG, "Ending long running calculation!")
+//
+////            }
+//            Log.d(TAG, "Started long running calculation...")
+//            withTimeout(3000L) {
+//                for (i in 30..40) {
+//                    if (isActive) {
+//                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
+//                    }
 //                }
-//                Log.d(TAG, "Ending long running calculation!")
-
 //            }
-            Log.d(TAG, "Started long running calculation...")
-            withTimeout(3000L) {
-                for (i in 30..40) {
-                    if (isActive) {
-                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
-                    }
-                }
-            }
-
-            Log.d(TAG, "Ending long running calculation!")
-        }
+//
+//            Log.d(TAG, "Ending long running calculation!")
+//        }
 //        runBlocking {
 ////            job.join()
 //            delay(2000)
@@ -89,12 +106,23 @@ class MainActivity : AppCompatActivity() {
 //
 //        }
     }
-
-    fun fib(n: Int): Long {
-        return when (n) {
-            0 -> 0
-            1 -> 1
-            else -> fib(n - 1) + fib(n - 2)
-        }
+    suspend fun networkCall1() :String {
+        delay(3000L)
+        return "Answer1"
     }
+
+    suspend fun networkCall2() :String {
+        delay(5000L)
+        return "Answer2"
+    }
+
+//    fun fib(n: Int): Long {
+//        return when (n) {
+//            0 -> 0
+//            1 -> 1
+//            else -> fib(n - 1) + fib(n - 2)
+//        }
+//    }
+
+
 }

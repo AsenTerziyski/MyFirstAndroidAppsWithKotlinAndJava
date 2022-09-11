964526424004fa3e5379d3ffe326332bcd92c60a
AsenTerziyski <asenterziyski@gmail.com>
2022-09-11 09:48:57 +0300

    Add part 1 of Morty App

---
+++ MortyApp/app/src/main/java/bg/asentt/mortyapp/network/Character.kt
@@ -0,0 +1,11 @@
+package bg.asentt.mortyapp.network
+
+import com.squareup.moshi.Json
+
+data class Character(
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "image")
+    val image: String
+)
+
---
+++ MortyApp/app/src/main/java/bg/asentt/mortyapp/network/CharacterResponse.kt
@@ -0,0 +1,8 @@
+package bg.asentt.mortyapp.network
+
+import com.squareup.moshi.Json
+
+data class CharacterResponse(
+    @Json(name = "results")
+    val result: List<Character>
+)

d6d2505613a40b624980e2fae551fb6c576d3b35
AsenTerziyski <asenterziyski@gmail.com>
2022-10-09 09:04:06 +0300

    Add description fragment project

---
+++ DescriptionScreen/app/src/main/java/bg/asentt/descriptionscreen/MainActivity.kt
@@ -0,0 +1,16 @@
+package bg.asentt.descriptionscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//https://stackoverflow.com/questions/35762006/how-to-have-edittext-with-border-in-android-lollipop
+//https://stackoverflow.com/questions/3285412/whats-the-best-way-to-limit-text-length-of-edittext-in-android
+//https://www.meghandev.io/post/give-rounded-corners-to-views-and-layouts-in-android
+//https://constraintlayout.com/basics/create_chains.html
+//https://stackoverflow.com/questions/30440031/how-to-make-edittext-hint-right-to-left-sided
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
